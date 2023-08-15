85f4331beda7f9d55952b7956c0cdd3191337df3
Beatrizresteves <Beatrizresteves@github.com>
2023-08-14 22:18:26 -0300

    Add projetos

---
+++ robozinho_mouse.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+pyautogui.keyDown('alt')
+pyautogui.press(['tab'])
+pyautogui.keyUp('alt')
+
+pyautogui.move(0, 300)
+pyautogui.click
+pyautogui.drag(xOffset=200, yOffset=200, duration=2)
---
+++ robozinho_teclado.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+pyautogui.keyDown('alt')
+pyautogui.press(['tab'])
+pyautogui.keyUp('alt')
+
+pyautogui.press(['left', 'left', 'left'])
+
+pyautogui.write("Se inscreba no meu canal")
